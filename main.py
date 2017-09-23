2d2b9d642d660189986cf3924184b5d977bb6dcd
Mathieu <mathieu.vallee1998@gmail.com>
2017-09-23 14:11:00 +0200

    1°séparation de la fonction en 2
    2°utilisation de la technique de la normale pour faire l'homotétie et créer les mesh supplémentaire autours de la surface de l'aile
    
    3°changement pour que la couche suivante dépende de la couche juste avant et pas de celle initiale
    
    4°rectification des fonctions generate_layers et adébut de la fonction generate_meshes renvoyant le tableau avec les indices des triangles qui sont dans l'aile (pour les conditions initiales) et le liste de tout les autres triangles

--- main.py
+++ main.py
@@ -5,50 +5,73 @@ import meshpy as mp
 import scipy.spatial as scsp
 
 
-def generate_wing(a, c, d, n):
+def function_wing(a, c, d, theta):
     r = abs(complex(c - a, d))
+    return (
+    0.5 * ((complex(c, d) + r * exp(complex(0, theta))) + (a * a) / (complex(c, d) + r * exp(complex(0, theta)))))
+
+
+def generate_wing(a, c, d, n):
     theta = np.linspace(0, 2 * np.pi, n)
-    z = np.zeros(n) * complex(0, 1)
+    wing_complex = np.zeros(n) * complex(0, 1)
     x = np.zeros(n)
     y = np.zeros(n)
     wing = np.zeros((n, 2))
     for k in range(n):
-        z[k] = 0.5 * (
-        (complex(c, d) + r * exp(complex(0, theta[k]))) + (a * a) / (complex(c, d) + r * exp(complex(0, theta[k]))))
-        x[k] = (z[k].real)
-        y[k] = (z[k].imag)
+        wing_complex[k] = function_wing(a, c, d, theta[k])
+        x[k] = (wing_complex[k].real)
+        y[k] = (wing_complex[k].imag)
         wing[k] = [x[k], y[k]]
-    return (z, x, y, wing)
+    return (wing_complex, x, y, wing)
 
 
-def generate_mesh(wing_complex, nbr_of_meshes):
+def generate_layers(wing_complex, nbr_of_layers):
     n = len(wing_complex)
-    points = np.zeros((nbr_of_meshes * n, 2))
+    points_complex = np.zeros((nbr_of_layers, n)) * complex(0, 1)
+    ## injecter les points de la matrice wing_complex dans la première ligne de la matrice points
     for k in range(n):
-        points[k] = [wing_complex[k].real,wing_complex[k].imag]
-    for h in range(1,nbr_of_meshes):
-        rapport = np.exp(h * h / (nbr_of_meshes * nbr_of_meshes)) - 1
+        points_complex[0, k] = wing_complex[k]
+    ## calculer les points à ajouter étant des translation de chaque point selon la normale entre 2 points:
+    ## créer un maillage plus précis sur les bords de l'aile
+    for h in range(1, nbr_of_layers):
+        rapport = np.exp(h * h / (nbr_of_layers * nbr_of_layers)) - 1
         for k in range(n - 1):
-            zprime = complex(points[n*(h-1) + k + 1,0],points[n*(h-1) + k + 1,1]) - complex(points[n*(h-1) + k,0],points[n*(h-1) + k,1])
+            zprime = points_complex[h - 1, k + 1] - points_complex[h - 1, k]
             argu = phase(zprime)
-            zc = exp(complex(0, np.pi / 2 + argu))
-            zf = (complex(points[n*(h-1) + k + 1,0],points[n*(h-1) + k + 1,1]) + complex(points[n*(h-1) + k,0],points[n*(h-1) + k,1])) / 2
-            z0 = zf + zc*rapport
-            points[h * n + k] = [z0.real, z0.imag]
-        zprime = complex(points[n*h,0],points[n*h,1]) - complex(points[n*(h-1) + n - 1,0],points[n*(h-1) + n-1,1])
+            z_step = exp(complex(0, np.pi / 2 + argu))
+            z_moy = (points_complex[h - 1, k + 1] + points_complex[h - 1, k]) / 2
+            # argu = phase(z_moy)
+            # z_step = exp(complex(0, np.pi / 2 + argu))
+            z0 = z_moy + z_step * rapport
+            points_complex[h, k] = z0
+        ## traitement du cas final: placer un point translaté entre le point de coordonée [h-1,0] et [h-1,n-1]
+        zprime = points_complex[h - 1, 0] - points_complex[h - 1, n - 1]
         argu = phase(zprime)
-        zc = exp(complex(0, np.pi / 2 + argu))
-        zf = (complex(points[n*h,0],points[n*h,1]) + complex(points[n*(h-1) + n - 1,0],points[n*(h-1) + n-1,1])) / 2
-        z0 = zf + zc * rapport
-        points[(h + 1) * n - 1] = [z0.real, z0.imag]
+        z_step = exp(complex(0, np.pi / 2 + argu))
+        z_moy = (points_complex[h - 1, 0] + points_complex[h - 1, n - 1]) / 2
+        # argu = phase(z_moy)
+        # z_step = exp(complex(0, np.pi / 2 + argu))
+        z0 = z_moy + z_step * rapport
+        points_complex[h, n - 1] = z0
+    ##transformation de la matrice points_complex en points: array de taille (n*nbr_of_layers,2) listant les coordonées dans R² des points complexes
+    points_complex = np.reshape(points_complex, n * nbr_of_layers)
+    points = np.zeros((n * nbr_of_layers, 2))
+    for k in range(n * nbr_of_layers):
+        points[k] = [points_complex[k].real, points_complex[k].imag]
+
     return (points)
 
 
-a, b, c, p0 = generate_wing(5, 0.5, 0.4, 100)
-p = generate_mesh(a, 10)
-tri1 = scsp.Delaunay(p)
-plt.triplot(p[:, 0], p[:, 1], tri1.simplices.copy())
-plt.plot(p[:, 0], p[:, 1], 'o')
+def generate_meshes(a, c, d, n, nbr_of_layers):
+    wing_complex, x, y, wing = generate_wing(a, c, d, n)
+    points = generate_layers(wing_complex,nbr_of_layers)
+    meshes = scsp.Delaunay(points)
+    return (points,meshes)
 
+
+
+p0, tri1 = generate_meshes(5, 0.5, 0.4, 100 , 10)
+plt.triplot(p0[:, 0], p0[:, 1], tri1.simplices.copy())
+plt.plot(p0[:, 0], p0[:, 1], 'o')
 
 plt.show()
